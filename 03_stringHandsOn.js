c504b5628c95d5cb5b8a30e9f5464fb35bbfd8f0
rupalibichukale13 <160692463+rupalibichukale13@users.noreply.github.com>
2024-03-13 08:27:51 -0900

    String Functions
    String Functions

---
+++ 03_stringHandsOn.js
@@ -0,0 +1,53 @@
+
+function stringHandsOn(){
+    var sentence = "   Hey you are doing good, keep it up   ";
+    console.log("========== STEP 1 ==========");
+    console.log("Given string is:" ,sentence);
+}
+stringHandsOn();
+
+console.log("========== STEP 2 ==========");
+
+var sentence = "   Hey you are doing good, keep it up   ";
+var totalChars = sentence.length;
+console.log(`Length of Given String is: ${totalChars}`);
+
+console.log("========== STEP 3 ==========");
+var sentenceTrim = sentence.trim();
+var sentenceTrimLength= sentenceTrim.length;
+console.log(`String after trim is:${sentenceTrim } and its length is: ${sentenceTrimLength}`);
+
+console.log("========== STEP 4 ==========");
+console.log (`Total number of extra Spaces removed in third step is: ${totalChars- sentenceTrimLength}`);
+
+
+console.log("========== STEP 5 ==========");
+var firstChar= sentenceTrim.charAt(0)
+var lastChar = sentenceTrim.charAt(sentenceTrimLength -1);
+console.log(`First Character After trim  is:${ firstChar}` );
+console.log(`Last Character After Trim is:${lastChar}`);
+
+console.log("========== STEP 6 ==========");
+//var words = sentenceTrim.split();
+//console.log(words);
+//console.log(`count of total words after trim is:${words.length}`);
+ var resultValue = sentenceTrim.split(" ")
+ console.log(resultValue);
+ console.log(`Total count of  words after strim are:${resultValue.length}`);
+
+ console.log("========== STEP 7 ==========");
+ var sentenceIndex = sentenceTrim.indexOf("good");
+console.log(`Index of the word "good" is:${sentenceIndex} `);
+
+console.log("========== STEP 8 ==========");
+var substring  = sentenceTrim.substr(22);
+console.log(`substring starting from index 22 is:${substring}`);
+
+console.log("========== STEP 9 ==========");
+var stringEnd = sentenceTrim.endsWith('up');
+console.log(`String ends with word "up" after step 3 is:${stringEnd}`);
+
+console.log("========== STEP 10 ==========");
+var stringStart = sentenceTrim.startsWith('Hey');
+console.log (` String start with word "Hey" after trimming is:${stringStart}`);
+
--- o4_string.js
+++ o4_string.js
@@ -33,22 +33,59 @@ var greet = "Good Morning";
  var  greetInUpperCase =  greet.toUpperCase();
  console.log(`${greet} in upper case is: ${greetInUpperCase}`);
 
- console.log(`========== trim ==========`);
- var greet = "   Good Morning    ";
- console.log(`${greet} its length is: ${greet.length}`);
- var length = greet.length;
- console.log(`${greet} its length is: ${length}`);
+console.log("========= trim =========");
+ var greet = "Good Morning";
+ var lengthBeforeTrim = greet.length;
+ console.log(` ${greet} it's length is : ${lengthBeforeTrim} `);
+ 
+ var greetAfterTrim = greet.trim();
+ var lengthAfterTrim = greetAfterTrim.length;
+ console.log(` ${greetAfterTrim} it's length is : ${lengthAfterTrim} `);
+
+
+console.log(`Total spaces removed is: ${lengthBeforeTrim-lengthAfterTrim}` );
  
 
 
  console.log(`=========== includes ==========`);
  var greet = "Good Morning";
- var greetIncludes = greet.includes();
- console.log(`${greet} includes is: ${greetIncludes}`);
+ var result = greet.includes("nin");
+ console.log(` Is String ${greet} includes word or char "nin": ${result}`);
+
+
+ console.log(`=========== Search ==========`);
+var greet = "Good Morning";
+var result = greet.search ("Morning");
+console.log(` "Morning" is available at ${result} `);
+
+
 
  console.log(`========== Slice Method ==========`);
  var greet = "Good Morning";
- var greetSlice = greet.slice();
- console.log(greet.slice(0,5));
+ var result = greet.slice(3,10);
+ console.log(`Slice is ${result}`);
 
+ console.log(`========== Split ==========`);
+ var greet = "Good Morning";
+ var resultValue = greet.split(" ")
+ console.log(resultValue);
+ console.log(`Total words are:${resultValue.length}`);
+
+ 
+
+// Write a function with name totalWord() with one arguments
+//  "I am happy Buddy" 
+//  "I am learning JS the language of internet"
+// and this function should return the total numbers of words
+
+function totalWord(sentence){
+    var words = sentence.split(" ");
+    var totalWords = words.length;
+    return totalWords
+}
+var totalWords = totalWord("I am happy Buddy");
+console.log("I am happy Buddy", Total number od words is: ${totalWords});
+
+var totalWords = totalWord("I am learning JS the language of internet");
+console.log("I am learning JS the language of internet", Total number od words is: ${totalWords});
 
