6e44abbb1e4cea5c88661ee73947d092d436f9ff
rupalibichukale13 <160692463+rupalibichukale13@users.noreply.github.com>
2024-03-25 08:56:42 -0900

    for loop Assignment
    for loop Assignment

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,51 @@
+
+
+
+console.log(`------------------ WAP to print numbers from 5 t0 15 ------------------`);
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+    
+}
+console.log(`------------------ WAP to print numbers from 50 t0 40 ------------------`);
+for (let index = 50; index >=40; index--) {
+  console.log(index);
+    
+}
+
+console.log(`------------------ WAP to first 15 odd numbers ------------------`);
+for (let index = 1; index <=30; index = index+2) {
+   console.log(index);
+    
+}
+
+console.log(`------------------ WAP to first 10 even numbers ------------------`);
+for (let index = 2; index <=20; index = index+2) {
+console.log(index);
+  
+}
+
+console.log(`------------------ WAP to print table of 5 ------------------`);
+const number = 5;
+for (let index = 1; index <=10;  ++index) {
+const result = number * index;
+console.log(` ${result}`);
+  
+}
+
+
+console.log(`------------------ WAP to print table of 10 ------------------`);
+const num = 10;
+for (let index = 1; index <=10;  index++) {
+const result = num * index;
+console.log(` ${result}`);
+  
+}
+
+
+console.log(`------------------ WAP to print table of 10 in reverse order ------------------`);
+const number1  = 10;
+for (let index = 10; index >=1;  index--) {
+const result = number1 * index;
+console.log(` ${result}`);
+  
+}
