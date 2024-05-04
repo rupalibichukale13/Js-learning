9ea89e125dfa65052f270a308f6e311fec4e6788
rupalibichukale13 <160692463+rupalibichukale13@users.noreply.github.com>
2024-05-04 10:01:39 -0900

    discount calculator assignment
    discount calculator assignment

--- 06_forLoop.js
+++ 06_forLoop.js
@@ -32,3 +32,5 @@ console.log(index);
    
 }
 
+
+
--- 28_script.js
+++ 28_script.js
@@ -33,3 +33,31 @@ techStackElement.innerHTML = "My Expertise in UI";
 
 console.log('Changing CSS ');
 techStackElement.style.color = "orange";
+
+console.log('Creating an element');
+const h2Element = document.createElement("h2");
+const textNodeProject = document.createTextNode("Project Name: College Management System");
+h2Element.appendChild(textNodeProject);
+h2Element.style.color="green";
+
+const projectNodeDiv = document.querySelector('.project');
+projectNodeDiv.appendChild(h2Element);
+
+console.log('Remove an element');
+const skillUlNode = document.querySelector('#skill');
+const JsLiNode = document.querySelector('#js');
+skillUlNode.removeChild(JsLiNode);
+
+function eventClick(){
+    console.log("Hey.. he clicked me...");
+}
+const addButton = document.querySelector('#add');
+addButton.addEventListener('click', ()=>{
+  const num = prompt("Enter the valid number to perform square", 1);
+  const elementPara = document.createElement('p');
+  const textNodeSquare = document.createTextNode(`Number ${num} and it's square is ${num*num}`)
+  elementPara.style.color= 'orange';
+  elementPara.appendChild(textNodeSquare)
+  const squareResultElement = document.querySelector('.square-result');
+  squareResultElement.appendChild(elementPara);
+});
